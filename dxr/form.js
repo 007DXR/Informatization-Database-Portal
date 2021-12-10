27df78acb2c10225c0ec88d94b9e4f72113850ea
Xinran Dong <81548653+007DXR@users.noreply.github.com>
2021-12-10 09:35:54 +0800

    Add files via upload

---
+++ dxr/form.js
@@ -0,0 +1,106 @@
+
+function Listener3(level) {
+    document.getElementById("select3").onclick = function () {
+        for (var i in level) {
+            var name1 = level[i].name
+
+            if (document.getElementById(name1).selected) {
+                // alert(name1)
+                document.getElementById("level3").value = name1
+              
+
+            }
+        }
+    }
+}
+function Listener2(level) {
+    document.getElementById("select2").onclick = function () {
+        for (var i in level) {
+            var name1 = level[i].name
+
+            if (document.getElementById(name1).selected) {
+                // alert(name1)
+                document.getElementById("level2").value = name1
+                var select2 = document.getElementById("select3")
+                while (select2.length > 0) {
+                    select2.remove(0);
+                    // alert(select2.length)
+                }
+                var y = document.createElement('option');
+                y.text = "新建"
+                select2.add(y, null)
+                for (var j in level[i].children) {
+                    var name2 = level[i].children[j].name
+                    var y = document.createElement('option');
+                    y.id = name2
+                    y.text = name2
+                    select2.add(y, null)
+                }
+                Listener3(level[i].children)
+
+            }
+        }
+    }
+}
+function Listener1(level1) {
+    document.getElementById("select1").onclick = function () {
+        for (var i in level1) {
+            var name1 = level1[i].name
+
+            if (document.getElementById(name1).selected) {
+                // alert(name1)
+                document.getElementById("level1").value = name1
+                var select2 = document.getElementById("select2")
+                while (select2.length > 0) {
+                    select2.remove(0);
+                    // alert(select2.length)
+                }
+                var y = document.createElement('option');
+                y.text = "新建"
+                select2.add(y, null)
+                for (var j in level1[i].children) {
+                    var name2 = level1[i].children[j].name
+                    var y = document.createElement('option');
+                    y.id = name2
+                    y.text = name2
+                    select2.add(y, null)
+                }
+                Listener2(level1[i].children)
+
+            }
+        }
+    }
+}
+Listener1(filterData.children)
+document.getElementById("submit").onclick = function () {
+    var value1 = document.getElementById("level1").value
+    var value2 = document.getElementById("level2").value
+    var value3 = document.getElementById("level3").value
+    let flag = 0
+    pos=filterData.children
+    for (var i in pos) {
+        var name1 = pos[i].name
+        if (value1 == name1) {
+            pos=pos[i].children
+            for (var j in pos){
+                if (value2==pos[j].name){
+                    pos=pos[j].children
+                    for (var k in pos){
+                        if (value3==pos[k].name){
+                            pos=pos[k].children
+                            pos.push({asdf:23})
+                            flag = 1
+                            alert("scc")
+                            console.log(pos)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // if (!flag) {
+    //     filterData.children.push({ name: value1, children: [{ name: value2, children: [{ name: value3,  }] }] })
+    //     return
+    // }
+    // console.log(filterData)
+}
---
+++ dxr/treemap.js
@@ -0,0 +1,75 @@
+ROOT_PATH = "/data.json"
+const uploadedDataURL = ROOT_PATH
+
+treemapChart = echarts.init(document.getElementById('treemap'));
+// console.log(filterData)
+
+var treemapOption = {
+  title: {
+    text: '信息化程度评价指标体系',
+    subtext: '作者',
+    left: 'leafDepth'
+  },
+
+  tooltip: {
+    formatter: function (info) {
+      var value = info.value;
+      var treePathInfo = info.treePathInfo;
+      var treePath = [];
+      for (var i = 1; i < treePathInfo.length; i++) {
+        treePath.push(treePathInfo[i].name);
+      }
+      return [
+        '<div class="tooltip-title">' +
+        treePath.join('/') +
+        '</div>',
+        '得分: ' + value
+      ].join('');
+    }
+  },
+  series: [
+    {
+      name: '评价指标体系',
+      type: 'treemap',
+      visibleMin: 1,
+      data: filterData.children,
+      leafDepth: 1,
+      levels: [
+        {
+          itemStyle: {
+            borderColor: '#555',
+            borderWidth: 4,
+            gapWidth: 4
+          }
+        },
+        {
+          colorSaturation: [0.3, 0.6],
+          itemStyle: {
+            borderColorSaturation: 0.7,
+            gapWidth: 2,
+            borderWidth: 2
+          }
+        },
+        {
+          colorSaturation: [0.3, 0.5],
+          itemStyle: {
+            borderColorSaturation: 0.6,
+            gapWidth: 1
+          }
+        },
+        {
+          colorSaturation: [0.3, 0.5]
+        }
+      ]
+    }
+  ]
+}
+
+treemapChart.setOption(treemapOption);
+
+treemapChart.on('click', function (params){
+  console.log(params)
+  lineChart.setOption({series:compute(params.data),"title": [
+    {
+        "text": params.name}]})
+});
